02345d3d5199e2fe37eff82c376eedb9cce16a0a
Virinchi Rao Manthani <virinchirao@virinchirao-macbookpro.roam.corp.google.com>
2019-07-30 15:15:23 +0530

    Initial commit

---
+++ app/src/main/java/com/samples/retrofitwithpicasso/GetDataService.java
@@ -0,0 +1,13 @@
+package com.samples.retrofitwithpicasso;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/photos")
+    Call<List<RetroPhotoModel>> getAllPhotos();
+}
---
+++ app/src/main/java/com/samples/retrofitwithpicasso/MainActivity.java
@@ -0,0 +1,57 @@
+package com.samples.retrofitwithpicasso;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDoalog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDoalog = new ProgressDialog(MainActivity.this);
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<RetroPhotoModel>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<RetroPhotoModel>>() {
+            @Override
+            public void onResponse(Call<List<RetroPhotoModel>> call, Response<List<RetroPhotoModel>> response) {
+                progressDoalog.dismiss();
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroPhotoModel>> call, Throwable t) {
+                progressDoalog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*Method to generate List of data using RecyclerView with custom adapter*/
+    private void generateDataList(List<RetroPhotoModel> photoList) {
+        recyclerView = findViewById(R.id.customRecyclerView);
+        adapter = new CustomAdapter(this,photoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
